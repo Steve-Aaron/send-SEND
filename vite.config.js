2614e6696ac44bd37c55203b2792202b4a2dbcd4
Steve Aaron <steve@campaigning.digital>
2026-03-05 17:47:08 +0000

    fixe vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/', // Replace with your exact repo name
+  base: '/',
   plugins: [
     tailwindcss(),
     react(),
