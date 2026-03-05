727962e991c713670cdb6c8a0762f39e9c8fa09f
Steve Hawkins <steveknoxdigi@Steves-MacBook-Air.local>
2026-03-05 13:59:38 +0000

    First commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/send-SEND/', // Replace with your exact repo name
   plugins: [
     tailwindcss(),
     react(),
